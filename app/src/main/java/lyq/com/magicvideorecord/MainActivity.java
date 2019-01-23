74deda87fbdf9ab53f9b49ae48be66fd206a89e1
sunshiny_lyq <liyuqing0423@163.com>
2019-01-24 07:21:10 +0800

    修改

--- app/src/main/java/lyq/com/magicvideorecord/MainActivity.java
+++ app/src/main/java/lyq/com/magicvideorecord/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity implements SensorControler.C
         mSensorControler.setCameraFocusListener(this);
         initView();
         initFilter();
+
     }
 
 
